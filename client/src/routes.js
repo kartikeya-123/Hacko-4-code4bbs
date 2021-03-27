0bab7151e255e22d16c9d6a579fa06687ae80ef2
Mintu Jupally <mintu.jupally@gmail.com>
2021-03-27 10:22:45 +0530

    Corrected routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,22 +3,19 @@ import MainLayout from "./layouts/MainLayout";
 import LoginView from "./views/auth/LoginView";
 import NotFoundView from "./views/errors/NotFoundView";
 
+import DashboardLayout from "./layouts/DashboardLayout";
+import DashboardView from "./views/reports/DashboardView";
+
 const setUserAsProps = (user, cookies) => {
   return [
-    // {
-    //   path: 'app',
-    //   element: <DashboardLayout user={user} cookies={cookies} />,
-    //   children: [
-    //     { path: 'account', element: <AccountView user={user} /> },
-    //     { path: 'customers', element: <CustomerListView user={user} /> },
-    //     { path: 'dashboard', element: <DashboardView user={user} /> },
-    //     { path: 'sports', element: <ProductListView user={user} /> },
-    //     { path: 'sports/:id', element: <EquipmentView user={user} /> },
-    //     { path: 'settings', element: <SettingsView /> },
-    //     { path: 'complaints', element: <ComplaintView user={user} /> },
-    //     { path: '*', element: <Navigate to="/404" /> },
-    //   ],
-    // },
+    {
+      path: "app",
+      element: <DashboardLayout user={user} cookies={cookies} />,
+      children: [
+        { path: "dashboard", element: <DashboardView user={user} /> },
+        { path: "*", element: <Navigate to="/404" /> },
+      ],
+    },
     {
       path: "/",
       element: <MainLayout />,
