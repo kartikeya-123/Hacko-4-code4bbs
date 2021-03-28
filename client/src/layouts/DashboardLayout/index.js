f9200317adc745cb13d868e9ee5de20542a00bf3
kartikeya-123 <56500864+kartikeya-123@users.noreply.github.com>
2021-03-28 11:46:44 +0530

    small change

--- client/src/layouts/DashboardLayout/index.js
+++ client/src/layouts/DashboardLayout/index.js
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core';
 import NavBar from './NavBar';
 import TopBar from './TopBar';
-
+import { GoogleLogout } from 'react-google-login';
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: theme.palette.background.default,
